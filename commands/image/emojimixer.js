6c7e8d2ee41cdeddb9b05c770b423684a6ec30cc
Hitis24 <157496309+Hitis24@users.noreply.github.com>
2024-02-20 17:58:10 +0000

    pt 2

--- commands/image/emojimixer.js
+++ commands/image/emojimixer.js
@@ -2,23 +2,23 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   name: 'emojimixer',
-  aliases: ['mixemoji'], // Add the alias here
+  aliases: ['mixemojis'], // Add the alias here
   description: 'Combine two different emojis',
-  run: async (client, message, args) => {
-    await message.deferReply({ ephemeral: true });
+  run: async (client, interaction, args) => {
+    await interaction.deferReply({ ephemeral: true });
 
     const eString = args[0];
     const input = onlyEmoji(eString);
 
     if (input.length !== 2) {
       const response = 'Please provide exactly two emojis.';
-      return await message.editReply({ content: response });
+      return await interaction.editReply({ content: response });
     }
 
     const response = `One or both of these emojis (\`${input}\`) are not supported. Keep in mind that gestures (i.e., thumbsup) and custom server emojis are not supported.`;
 
     const output = await superagent.get('https://tenor.googleapis.com/v2/featured').query({
-      key: 'AIzaSyBEUenxJYn75oZ7X9gVpqK-IkfUVCGy18w',
+      key: '',
       contentfilter: 'high',
       media_filter: 'png_transparent',
       component: 'proactive',
@@ -27,13 +27,13 @@ module.exports = {
     }).catch(err => {});
 
     if (!output || !output.body.results[0]) {
-      return await message.editReply({ content: response });
+      return await interaction.editReply({ content: response });
     } else if (eString.startsWith('<') || eString.endsWith('>')) {
-      return await message.editReply({ content: response });
+      return await interaction.editReply({ content: response });
     }
 
     const embed = new MessageEmbed().setColor('BLURPLE').setImage(output.body.results[0].url);
 
-    await message.editReply({ embeds: [embed] });
+    await interaction.editReply({ embeds: [embed] });
   }
 };
