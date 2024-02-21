a542a1119fbb9217a20f04f2964d153d4daa6fb0
Hitis24 <157496309+Hitis24@users.noreply.github.com>
2024-02-21 08:22:35 +0000

    pt 4

--- SlashCommands/images/emojimixer.js
+++ SlashCommands/images/emojimixer.js
@@ -1,38 +1,48 @@
-const { MessageEmbed } = require('discord.js');
+const { Client, MessageEmbed, CommandInteraction } = require("discord.js");
 const superagent = require('superagent');
 const { onlyEmoji } = require('emoji-aware');
 
 module.exports = {
-    name: 'emojimixer',
-    description: 'Combine two different emojis',
-    async execute(interaction) {
-        await interaction.deferReply({ ephemeral: true });
-
-        const eString = interaction.options.getString('emojis');
-        const input = onlyEmoji(eString);
-
-        if (input.length !== 2) {
-            const response = 'Please provide exactly two emojis.';
-            return await interaction.editReply({ content: response });
-        }
-
-        const response = `One or both of these emojis (\`${input}\`) are not supported. Keep in mind that gestures (i.e., thumbsup) and custom server emojis are not supported.`;
-
-        const output = await superagent.get('https://tenor.googleapis.com/v2/featured').query({
-            key: 'AIzaSyBEUenxJYn75oZ7X9gVpqK-IkfUVCGy18w',
-            contentfilter: 'high',
-            media_filter: 'png_transparent',
-            component: 'proactive',
-            collection: 'emoji_kitchen_v5',
-            q: input.join('')
-        }).catch(err => {});
-
-        if (!output || !output.body.results[0] || eString.startsWith('<') || eString.endsWith('>')) {
-            return await interaction.editReply({ content: response });
-        }
-
-        const embed = new MessageEmbed().setColor('BLURPLE').setImage(output.body.results[0].url);
-
-        await interaction.editReply({ embeds: [embed] });
+  name: 'emojimixer',
+  description: 'Combine two different emojis',
+  type: 'CHAT_INPUT',
+  options: [
+    {
+      name: 'emojis',
+      description: 'The emojis to combine',
+      type: 'STRING',
+      required: true,
     }
+  ],
+
+  run: async (client, interaction, args) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    const eString = interaction.options.getString('emojis');
+    const input = onlyEmoji(eString);
+
+    if (input.length !== 2) {
+      const response = 'Please provide exactly two emojis.';
+      return await interaction.editReply({ content: response });
+    }
+
+    const response = `One or both of these emojis (\`${input}\`) are not supported. Keep in mind that gestures (i.e., thumbsup) and custom server emojis are not supported.`;
+
+    const output = await superagent.get('https://tenor.googleapis.com/v2/featured').query({
+      key: 'AIzaSyBEUenxJYn75oZ7X9gVpqK-IkfUVCGy18w',
+      contentfilter: 'high',
+      media_filter: 'png_transparent',
+      component: 'proactive',
+      collection: 'emoji_kitchen_v5',
+      q: input.join('')
+    }).catch(err => {});
+
+    if (!output || !output.body.results[0] || eString.startsWith('<') || eString.endsWith('>')) {
+      return await interaction.editReply({ content: response });
+    }
+
+    const embed = new MessageEmbed().setColor('BLURPLE').setImage(output.body.results[0].url);
+
+    await interaction.editReply({ embeds: [embed] });
+  },
 };
