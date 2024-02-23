614aee6d0cd1cff7da344032e176748b0a88f459
Hitis24 <157496309+Hitis24@users.noreply.github.com>
2024-02-23 04:43:14 +0000

    ded2

--- SlashCommands/images/emojimixer.js
+++ SlashCommands/images/emojimixer.js
@@ -3,39 +3,49 @@ const superagent = require('superagent');
 const { onlyEmoji } = require('emoji-aware');
 
 module.exports = {
+  data: {
     name: 'emojimixer',
     description: 'Combine two different emojis',
-    async execute(message, args) {
-        try {
-            const eString = args.join(' ');
-            const input = onlyEmoji(eString);
+    options: [
+      {
+        name: 'emojis',
+        description: 'Two emojis to combine',
+        type: 'STRING',
+        required: true
+      }
+    ]
+  },
 
-            if (input.length !== 2) {
-                const response = 'Please provide exactly two emojis.';
-                return message.channel.send(response);
-            }
+  async execute(interaction) {
+    try {
+      const eString = interaction.options.getString('emojis');
+      const input = onlyEmoji(eString);
 
-            const response = `One or both of these emojis (\`${input}\`) are not supported. Keep in mind that gestures (i.e., thumbsup) and custom server emojis are not supported.`;
+      if (input.length !== 2) {
+        const response = 'Please provide exactly two emojis.';
+        return await interaction.reply({ content: response, ephemeral: true });
+      }
 
-            const output = await superagent.get('https://tenor.googleapis.com/v2/featured').query({
-                key: 'AIzaSyBEUenxJYn75oZ7X9gVpqK-IkfUVCGy18w',
-                contentfilter: 'high',
-                media_filter: 'png_transparent',
-                component: 'proactive',
-                collection: 'emoji_kitchen_v5',
-                q: input.join('')
-            });
+      const response = `One or both of these emojis (\`${input}\`) are not supported. Keep in mind that gestures (i.e., thumbsup) and custom server emojis are not supported.`;
 
-            if (!output || !output.body.results[0] || eString.startsWith('<') || eString.endsWith('>')) {
-                return message.channel.send(response);
-            }
+      const output = await superagent.get('https://tenor.googleapis.com/v2/featured').query({
+        key: 'YOUR_TENOR_API_KEY',
+        contentfilter: 'high',
+        media_filter: 'png_transparent',
+        component: 'proactive',
+        collection: 'emoji_kitchen_v5',
+        q: input.join('')
+      });
 
-            const embed = new MessageEmbed().setColor('BLURPLE').setImage(output.body.results[0].url);
-            message.channel.send({ embeds: [embed] });
-        } catch (error) {
-            console.error('Error handling command:', error);
-            // Handle the error appropriately, such as logging it or sending an error message to the user
-            message.channel.send('An error occurred while processing your request.');
-        }
+      if (!output || !output.body.results[0] || eString.startsWith('<') || eString.endsWith('>')) {
+        return await interaction.reply({ content: response, ephemeral: true });
+      }
+
+      const embed = new MessageEmbed().setColor('BLURPLE').setImage(output.body.results[0].url);
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+    } catch (error) {
+      console.error('Error handling interaction:', error);
+      await interaction.reply('An error occurred while processing your request.', { ephemeral: true });
     }
+  }
 };
